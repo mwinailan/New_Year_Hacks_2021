2b8da9987ca6f05a4656ee30a066f4895c2876ff
nemeiralal12 <62815678+nemeiralal12@users.noreply.github.com>
2021-01-02 22:46:20 -0600

    added the login page, signup page, connected to database, made the choose page, and one gif

---
+++ app/src/main/java/com/example/new_year_new_hacks/AnxietyGif.java
@@ -0,0 +1,14 @@
+package com.example.new_year_new_hacks;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnxietyGif extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anxiety_gif);
+    }
+}
--- app/src/main/java/com/example/new_year_new_hacks/MainActivity.java
+++ app/src/main/java/com/example/new_year_new_hacks/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.new_year_new_hacks;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private Button signUpButton;
@@ -41,6 +40,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(logIn);
             }
         });
-
     }
 }
